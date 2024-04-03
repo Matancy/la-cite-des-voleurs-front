cf12997d77edf47306dd3911d4ab36795f7f75db
oui_ <emilie.zammit29@gmail.com>
2024-04-03 09:22:34 +0200

    menu nav temporaire pour switcher entre les pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-import "./App.css";
-import Component from "./components/StoryLuckPage.tsx";
+import React, { useState } from "react";
+import CharacterCreation from "./components/CharacterCreation.tsx";
+import MainPage from "./components/MainPage.tsx";
+import StoryChoicePage from "./components/StoryChoicePage.tsx";
+import StoryFightPage from "./components/StoryFightPage.tsx";
+import StoryLuckPage from "./components/StoryLuckPage.tsx";
 
-function App() {
-    return <Component />;
-}
+const App = () => {
+    const [currentPage, setCurrentPage] = useState("accueil");
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case "character-creation":
+                return <CharacterCreation />;
+            case "story-choice":
+                return <StoryChoicePage />;
+            case "story-fight":
+                return <StoryFightPage />;
+            case "story-luck":
+                return <StoryLuckPage />;
+            case "main-page":
+                return <MainPage />;
+            default:
+                return <MainPage />;
+        }
+    };
+
+    return (
+        <div>
+            {renderPage()}
+            <nav>
+                <ul>
+                    <li onClick={() => setCurrentPage("accueil")}>Accueil</li>
+                    <li onClick={() => setCurrentPage("character-creation")}>
+                        Création de personnage
+                    </li>
+                    <li onClick={() => setCurrentPage("story-choice")}>
+                        Choix de l'histoire
+                    </li>
+                    <li onClick={() => setCurrentPage("story-fight")}>
+                        Combat d'histoire
+                    </li>
+                    <li onClick={() => setCurrentPage("story-luck")}>
+                        Chance de l'histoire
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+const Home = () => <MainPage />; // Définissez le composant Home ici
 
 export default App;
