f2abea79f0f540621b3ca6bc27a71b7e9dff8062
oui_ <emilie.zammit29@gmail.com>
2024-04-04 17:53:09 +0200

    better rules

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -20,7 +20,7 @@ const Rules = () => {
             </button>
             <h1 className="self-center mb-6 font-GrenzeGotisch text-white text-stroke-2px text-6xl">Règles</h1>
             <div className="w-2/3 self-center text-justify">
-                <h2><strong>Les Stats :D</strong></h2>
+                <h2 className="text-center font-bold">Les Stats :D</h2>
                 <br />
                 <p>
                     Pendant cette aventure, les capacités de votre personnage seront définies par ses statistiques que vous obtiendrez par des jets de dés. Je vous conseille de croire en l'âme des dés pour faciliter votre aventure et obtenir le maximum de points dans les 3 stats suivantes :
@@ -34,11 +34,11 @@ const Rules = () => {
                 <br />
                 <p>Mais ne vous inquiétez pas, vous aurez aussi un peu d'argent pour votre aventure qui pourrait être utile dans certaines conditions.</p>
                 <br />
-                <h2><strong>Les Combats  &gt;:(</strong></h2>
+                <h2 className="text-center font-bold">Les Combats  &gt;:(</h2>
                 <br />
                 <p>Parlons peu, parlons bien. Durant votre aventure, vous serez amené à combattre pour votre survie. Cela déclenchera une phase de combat où vous et votre ennemi lancerez 2D6 + votre habileté. Le perdant sera blessé et perdra 2 points d'endurance. Le premier qui réduit l'endurance de l'autre à 0 gagne. Le perdant meurt dans la souffrance et la honte.</p>
                 <br />
-                <h2><strong>La Chance ;)</strong></h2>
+                <h2 className="text-center font-bold">La Chance ;)</h2>
                 <br />
                 <p>Dans certaines actions que vous ferez, il faudra croire en l'âme des dés pour continuer en tentant votre chance. Lancez 2D6. Si votre jet est inférieur ou égal à votre chance, bravo, vous êtes quelqu'un de chanceux. Sinon, subissez le sadisme du maître du jeu  &gt;:).</p>
                 <br />
