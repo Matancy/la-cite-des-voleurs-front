e8125370c6e6d1d3b3a0916a2c047490843d196d
oui_ <emilie.zammit29@gmail.com>
2024-04-04 12:54:24 +0200

    a fixer : node non récupéré

--- src/components/StoryChoicePage.tsx
+++ src/components/StoryChoicePage.tsx
@@ -2,13 +2,45 @@ import React from "react";
 import storyData from "../assets/temp/choix.json";
 import HeaderStoryPage from "../widgets/HeaderStoryPage.tsx";
 import { useParams } from "react-router-dom";
+import { getNode } from "../model/callApi.ts";
+import { DirectLinkNode } from "../model/DirectLinkNode.ts";
+import { ChoicesNode } from "../model/ChoicesNode.ts";
+import { EndNode } from "../model/EndNode.ts";
+import { useNavigate } from "react-router-dom";
 
 const StoryChoicePage = () => {
     const { text, links, imageURL } = storyData;
     const params = useParams();
     const id = params.id;
+    const navigate = useNavigate();
 
     const playerMoney = 2; // Exemple de l'argent du joueur, à remplacer avec la valeur réelle récupérée depuis la base de données
+
+    let node: DirectLinkNode | ChoicesNode | EndNode;
+
+    async function callGetNode(id) {
+        let temp_node = await getNode(id);
+
+        if (temp_node.type) {
+            console.log(node.type);
+        } else {
+            console.log('node vide');
+        }
+
+        let type = temp_node.type;
+
+        if (type === "choice") {
+            node = temp_node as ChoicesNode;
+        } else if (type === "end") {
+            node = temp_node as EndNode;
+        } else if (type === "directLink") {
+            node = temp_node as DirectLinkNode;
+        } else {
+            navigate("./");
+        }
+    }
+
+    callGetNode(id);
 
     return (
         <div className="p-4 font-Inter text-xl flex flex-col background-old-page overflow-auto">
